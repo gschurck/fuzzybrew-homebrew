754a0de873667ac81114af7aef466859ea09ad24
Guillaume Schurck <21091232+gschurck@users.noreply.github.com>
2024-09-08 20:32:15 +0200

    feat: add fuzzybrew formula file

---
+++ fuzzybrew.rb
@@ -0,0 +1,17 @@
+class Fuzzybrew < Formula
+  desc "Fuzzy search for Homebrew packages"
+  homepage "https://github.com/gschurck/fuzzybrew"
+  url "https://github.com/gschurck/fuzzybrew/archive/v1.0.0.tar.gz"
+  sha256 "81aab02c7cef7f3efb86563ba02a4a3fdc765e1879d3b36b27d3ebdc92876411"
+  license "MIT"
+
+  depends_on "fzf"
+
+  def install
+    bin.install "fuzzybrew.sh" => "fuzzybrew"
+  end
+
+  test do
+    system "#{bin}/fuzzybrew", "--version"
+  end
+end
