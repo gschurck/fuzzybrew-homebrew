00aa4c35580236c9f2cbefad7c93edd176166ea9
Guillaume Schurck <21091232+gschurck@users.noreply.github.com>
2024-09-08 20:33:04 +0200

    feat: change formula version number

--- fuzzybrew.rb
+++ fuzzybrew.rb
@@ -1,7 +1,7 @@
 class Fuzzybrew < Formula
   desc "Fuzzy search for Homebrew packages"
   homepage "https://github.com/gschurck/fuzzybrew"
-  url "https://github.com/gschurck/fuzzybrew/archive/v1.0.0.tar.gz"
+  url "https://github.com/gschurck/fuzzybrew/archive/v0.1.0.tar.gz"
   sha256 "81aab02c7cef7f3efb86563ba02a4a3fdc765e1879d3b36b27d3ebdc92876411"
   license "MIT"
 
