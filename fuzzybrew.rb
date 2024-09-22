90d19b86e433eaf517bafb3964ced0356607745c
Guillaume Schurck <21091232+gschurck@users.noreply.github.com>
2024-09-22 20:34:50 +0200

    feat: update to v1.1.0

--- fuzzybrew.rb
+++ fuzzybrew.rb
@@ -1,8 +1,8 @@
 class Fuzzybrew < Formula
   desc "Fuzzy search for Homebrew packages"
   homepage "https://github.com/gschurck/fuzzybrew"
-  url "https://github.com/gschurck/fuzzybrew/archive/v1.0.1.tar.gz"
-  sha256 "d13716c922cf46f9f1df23348ecc4b2c2cd9527ea8b0c45ee4c003e3cdb9b69a"
+  url "https://github.com/gschurck/fuzzybrew/archive/v1.1.0.tar.gz"
+  sha256 "a3a5a112051075d4f60e7a4eda4e71d5ba8fd3fe969ff7976c30f639fc3a2ac0"
   license "MIT"
 
   depends_on "fzf"
